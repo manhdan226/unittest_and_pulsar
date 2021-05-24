3815c45cca9f985862524adb84f13f5e345acf29
Dan Nguyen Manh <manhdan226@gmail.com>
2021-05-24 23:33:34 +0700

    Cho item vao 1 ham roi goi vao class Test

--- test_unittest.py
+++ test_unittest.py
@@ -4,11 +4,16 @@ import logging
 
 dynamodb = boto3.resource('dynamodb', endpoint_url = "http://localhost:4566")
 table = dynamodb.Table('test_api')
-
 logging.basicConfig(level=logging.DEBUG, filename='runtime.log', filemode='w')
 
+def search_item(table):
+    scanResponse = table.scan(TableName='test_api')
+    item = scanResponse['Items'][0]
+    return item
+    
 class TestAPI(unittest.TestCase,item):
     def runTest(self):
+        item = search_item(table)
         test_id = item['id']
         self.assertIsInstance(test_id,str)
 
@@ -16,7 +21,8 @@ if __name__ == '__main__':
     while True:
         _count = table.count_item()
         if _count == 1:
-            scanResponse = table.scan(TableName='test_api')
-            item = scanResponse['Items'][0]
-            unittest.main(item)
+            unittest.main()
+            item = search_item(table) 
+            id = item['id']
+            table.delete_item(Key = {'id': id})
         
